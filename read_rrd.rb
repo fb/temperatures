74d4ce0e102743208fb9835903799f9faf5a99c6
Fabian Bartschke <fabian@bartschke.de>
2013-12-17 13:33:38 +0100

    relative paths

--- read_rrd.rb
+++ read_rrd.rb
@@ -1,4 +1,6 @@
 #!/usr/bin/env ruby
+Dir.chdir(File.dirname(__FILE__))
+
 require 'rubygems'
 require 'sinatra'
 
@@ -6,7 +8,7 @@ class ReadRRD < Sinatra::Base
   get '/temperatures/:time.xml', :provides => :xml do
     time = params[:time].match(/^\d+([hwmy]|(minute|hour|day|week|month|year)s?)$/) ? params[:time] : "1day"
     # TODO use all RRDs
-    defs = "DEF:a=/home/fb/temperatures/rrd/10.F9C18D020800.rrd:temperature:AVERAGE XPORT:a:Innen"
+    defs = "DEF:a=rrd/10.F9C18D020800.rrd:temperature:AVERAGE XPORT:a:inside"
     %x(rrdtool xport -s now-#{time} -e now #{defs})
   end
 end
